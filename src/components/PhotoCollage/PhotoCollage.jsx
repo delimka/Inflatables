4babd66bf1ab6e55fe60dea77e5e5cfcf69648aa
delimka <delima021@gmail.com>
2023-08-01 01:37:40 +0300

    UPD

--- src/components/PhotoCollage/PhotoCollage.jsx
+++ src/components/PhotoCollage/PhotoCollage.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { useTransition, animated } from 'react-spring';
 import './PhotoCollage.css';
 
 const PhotoCollage = ({ imageFilenames }) => {
+  console.log(imageFilenames); // Add this line to check the image filenames received
+
+  const transitions = useTransition(imageFilenames, {
+    from: { opacity: 0.5, }, 
+    enter: { opacity: 1, }, 
+    leave: { opacity: 0, },
+    config: { duration: 300 }, 
+    
+  });
+
   return (
     <div className='collage-container'>
-      {imageFilenames.map((image, index) => (
-        <div key={index} className='collage-item'>
-          <img src={image} alt={`photo_${index}`} className='collage-image' />
-        </div>
+      {transitions((style, image) => (
+        <animated.div style={style} key={image} className={"collage-item"}>
+          <img src={image} alt={`photo_${image}`} className='collage-image' />
+        </animated.div>
       ))}
     </div>
   );
--- src/constants/collageData/images.js
+++ src/constants/collageData/images.js
@@ -1,25 +1,62 @@
-import image_1 from '../../assets/collage/arches/image_1.jpg';
-import image_2 from '../../assets/collage/arches/image_2.jpg';
-import image_3 from '../../assets/collage/arches/image_3.jpg';
-import image_4 from '../../assets/collage/arches/image_4.jpg';
-import image_5 from '../../assets/collage/arches/image_5.jpg';
-import image_6 from '../../assets/collage/arches/image_6.jpg';
-import image_7 from '../../assets/collage/arches/image_7.jpg';
-import image_8 from '../../assets/collage/arches/image_8.jpg';
-import bottles_1 from '../../assets/collage/bottles/bottles_1.jpg';
-import bottles_2 from '../../assets/collage/bottles/bottles_2.jpg';
+// Import images for arches and bottles sub-options (Add more imports for other sub-options if needed)
+import arches1 from "../../assets/collage/arches/arches_1.jpg";
+import arches2 from "../../assets/collage/arches/arches_2.jpg";
+import arches3 from "../../assets/collage/arches/arches_3.jpg";
+import arches4 from "../../assets/collage/arches/arches_4.jpg";
+import arches5 from "../../assets/collage/arches/arches_5.jpg";
+import arches6 from "../../assets/collage/arches/arches_6.jpg";
+import arches7 from "../../assets/collage/arches/arches_7.jpg";
+import arches8 from "../../assets/collage/arches/arches_8.jpg";
 
-const image = {
-  image_1,
-  image_2,
-  image_3,
-  image_4,
-  image_5,
-  image_6,
-  image_7,
-  image_8,
-  bottles_1,
-  bottles_2
+import bottles1 from "../../assets/collage/bottles/bottles_1.jpg";
+import bottles2 from "../../assets/collage/bottles/bottles_2.jpg";
+import bottles3 from "../../assets/collage/bottles/bottles_3.jpg";
+import bottles4 from "../../assets/collage/bottles/bottles_4.jpg";
+import bottles5 from "../../assets/collage/bottles/bottles_5.jpg";
+import bottles6 from "../../assets/collage/bottles/bottles_6.jpg";
+import bottles7 from "../../assets/collage/bottles/bottles_7.jpg";
+import bottles8 from "../../assets/collage/bottles/bottles_8.jpg";
+
+import screens1 from "../../assets/collage/screens/screens_1.jpg";
+import screens2 from "../../assets/collage/screens/screens_2.jpg";
+import screens3 from "../../assets/collage/screens/screens_3.jpg";
+import screens4 from "../../assets/collage/screens/screens_4.jpg";
+import screens5 from "../../assets/collage/screens/screens_5.jpg";
+import screens6 from "../../assets/collage/screens/screens_6.jpg";
+import screens7 from "../../assets/collage/screens/screens_7.jpg";
+
+import balloons1 from "../../assets/collage/balloons/balloons_1.jpg";
+import balloons2 from "../../assets/collage/balloons/balloons_2.jpg";
+import balloons3 from "../../assets/collage/balloons/balloons_3.jpg";
+import ballonss4 from "../../assets/collage/balloons/balloons_4.jpg";
+import balloons5 from "../../assets/collage/balloons/balloons_5.jpg";
+import balloons6 from "../../assets/collage/balloons/balloons_6.jpg";
+import balloons7 from "../../assets/collage/balloons/balloons_7.jpg";
+
+import miniinflatables1 from "../../assets/collage/mini-inflatables/mini-inflatables_1.jpg";
+import miniinflatables2 from "../../assets/collage/mini-inflatables/mini-inflatables_2.jpg";
+import miniinflatables3 from "../../assets/collage/mini-inflatables/mini-inflatables_3.jpg";
+import miniinflatables4 from "../../assets/collage/mini-inflatables/mini-inflatables_4.jpg";
+import miniinflatables5 from "../../assets/collage/mini-inflatables/mini-inflatables_5.jpg";
+import miniinflatables6 from "../../assets/collage/mini-inflatables/mini-inflatables_6.jpg";
+import miniinflatables7 from "../../assets/collage/mini-inflatables/mini-inflatables_7.jpg";
+import miniinflatables8 from "../../assets/collage/mini-inflatables/mini-inflatables_8.jpg";
+
+const images = {
+  arches: [arches1, arches2, arches3, arches4, arches5, arches6, arches7, arches8],
+  bottles: [bottles1, bottles2, bottles3, bottles4, bottles5, bottles6, bottles7, bottles8],
+  screens: [screens1, screens2, screens3, screens4, screens5, screens6, screens7],
+  balloons: [balloons1, balloons2, balloons3, ballonss4, balloons5, balloons6, balloons7],
+  miniinflatables: [
+    miniinflatables1,
+    miniinflatables2,
+    miniinflatables3,
+    miniinflatables4,
+    miniinflatables5,
+    miniinflatables6,
+    miniinflatables7,
+    miniinflatables8,
+  ],
 };
 
-export default image;
+export default images;
